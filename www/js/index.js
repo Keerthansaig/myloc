c23062980eb0ae445821100dbb7dc1bffeb524cc
Keerthansaig <47209755+Keerthansaig@users.noreply.github.com>
2019-01-31 15:22:30 +0530

    www file
    uploaded www file

---
+++ www/js/index.js
@@ -0,0 +1,15 @@
+document.getElementById("getPosition").addEventListener("click",getPosition);
+function getPosition()
+
+{
+var watchID = 
+    navigator.geolocation.getCurrentPosition(onSuccess,onError);
+    function onSuccess(position)
+    {
+        alert('Latitude:'+position.coords.latitude+'\n'+'longitude:'+position.coords.longitude+'\n');
+    }
+    function onError(error)
+    {
+        alert('code:'   +error.code   +'\n'+ 'message:'+ error.message + '\n');
+    }
+    }
